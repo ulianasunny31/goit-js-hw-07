768e534a37a697b1efae2267b325fd64c1e6086b
Uliana Seredenko <uliana1331@gmail.com>
2023-11-08 12:32:32 +0200

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+const listMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `
+    <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+    `
+  )
+  .join("");
+galleryList.innerHTML = listMarkup;
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "top",
+  captionDelay: 250,
+});
